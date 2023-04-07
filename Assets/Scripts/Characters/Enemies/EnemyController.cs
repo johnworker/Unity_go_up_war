4c8dfa77dcc797f554fbdbeb758193f9ef770a31
hunte <besscomdark@gmail.com>
2023-04-07 21:03:08 +0800

    調整腳本

--- Assets/Scripts/Characters/Enemies/EnemyController.cs
+++ Assets/Scripts/Characters/Enemies/EnemyController.cs
@@ -13,9 +13,15 @@ public class EnemyController : MonoBehaviour
 
     [SerializeField] float moveRotationAngle = 25f;
 
+    private void Awake()
+    {
+    }
+
     private void OnEnable()
     {
         StartCoroutine(nameof(RandomMovingCoroutine));
+
+        Vector3 v3 = new Vector3(0, -90, 90);
     }
 
     IEnumerator RandomMovingCoroutine()
@@ -23,8 +29,6 @@ public class EnemyController : MonoBehaviour
         transform.position = Viewport.Instance.RandomEnemySpawnPosition(paddingX, paddingY);
 
         Vector3 targetPosition = Viewport.Instance.RandomTopHalfPosition(paddingX, paddingY);
-
-        Quaternion Rotation = Quaternion.Euler(moveRotationAngle, -90 , 90);
 
         while (gameObject.activeSelf)
         {
@@ -34,7 +38,8 @@ public class EnemyController : MonoBehaviour
                 // 繼續前往目標位置
                 transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
                 // 取得Y軸讓敵人移動旋轉
-                transform.rotation = Quaternion.AngleAxis((targetPosition - transform.position).normalized.x * moveRotationAngle, Vector3.up);
+                transform.rotation = Quaternion.Euler(0,-90 + ((targetPosition - transform.position).y * moveRotationAngle), 90);
+                // transform.rotation = Quaternion.AngleAxis((targetPosition - transform.position).x * moveRotationAngle, Vector3.up);
             }
             else
             {
